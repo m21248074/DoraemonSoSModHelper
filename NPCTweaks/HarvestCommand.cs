8ff1490c755ee8649c227f7eaa588158d5dcc2ad
Cavey <m.123051427@gmail.com>
2026-08-29 18:27:48 +0800

    feat: 新增 NPCTweaks 模組

---
+++ NPCTweaks/HarvestCommand.cs
@@ -0,0 +1,82 @@
+﻿using Define;
+
+namespace NPCTweaks
+{
+    public class HarvestCommand : AbstractCommand
+    {
+        public override int GetButton()
+        {
+            return 41;
+        }
+
+        public override string GetCommandName()
+        {
+            return SingletonMonoBehaviour<MasterManager>.Instance.TextMaster.GetText(1712070001);
+        }
+
+        public override Scene.Farm.StateEnum GetState()
+        {
+            return Scene.Farm.StateEnum.UI;
+        }
+
+        public override bool IsValid()
+        {
+            return true;
+        }
+
+        public override bool Execute(out ResponseModel response)
+        {
+            response = null;
+            bool result;
+            if (!IsValid())
+                return false;
+            else if (!DoWork())
+                result = false;
+            else
+            {
+                OutputMsg();
+                result = true;
+            }
+            return result;
+        }
+
+        private static bool DoWork()
+        {
+            UserModel user = SingletonMonoBehaviour<UserManager>.Instance.User;
+            bool result;
+            if (user == null)
+                result = false;
+            else
+            {
+                MapModel map = user.Farm.GetMap(Map.ID_FARM);
+                if (map == null)
+                    result = false;
+                else
+                {
+                    map.WetGrounds();
+                    for (int i = 0; i < map.GroundAreas.Length; i++)
+                    {
+                        for (int j = 0; j < map.GroundAreas[i].Grounds.Length; j++)
+                        {
+                            var ground = map.GroundAreas[i].Grounds[j];
+                            if (ground.HasCrop && ground.Crop.CanHarvest)
+                            {
+                                CropModel crop = map.GroundAreas[i].Grounds[j].Crop;
+                                user.ShippingBox.AddItem(new ItemModel(crop.Master.ItemId, crop.Quality, 1, -1));
+                                map.GroundAreas[i].Grounds[j].HarvestCrop();
+                            }
+                        }
+                    }
+                    result = true;
+                }
+            }
+            return result;
+        }
+    
+        private static void OutputMsg()
+        {
+            FarmTopUIController farmTopUIController = SingletonMonoBehaviour<UIManager>.Instance.GetUIController(UI.TypeEnum.FarmTop) as FarmTopUIController;
+            farmTopUIController.AddCharacterLogRequest(SingletonMonoBehaviour<MasterManager>.Instance.TextMaster.GetText(Work.Korobokkur.GetAssistLogTextId(Character.Korobokkur.AssistType.CropWork)), 201041010);
+        }
+    }
+}
---
+++ NPCTweaks/Plugin.cs
@@ -0,0 +1,78 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace NPCTweaks
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "dev.cavey.plugins.npctweaks";
+
+        public const string PLUGIN_NAME = "NPC Tweaks";
+
+        internal static new ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = base.Logger;
+
+            var harmony = new Harmony(PLUGIN_GUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(NpcModel))]
+        public static class NpcModel_Patch
+        {
+            [HarmonyPatch(nameof(NpcModel.ReceivePresent))]
+            [HarmonyPostfix]
+            public static void ReceivePresent_Postfix(NpcModel __instance)
+            {
+                Traverse.Create(__instance).Field("mIsReceivedPresent").SetValue(false);
+            }
+        }
+
+        [HarmonyPatch(typeof(FloorController))]
+        public static class FloorController_Patch
+        {
+            [HarmonyPatch("SetFurnitureCommands")]
+            [HarmonyPostfix]
+            public static void SetFurnitureCommands_Postfix(FurnitureController furniture_controller, FurnitureModel furniture_model, BeehiveController beehive_controller)
+            {
+                FurnitureMasterModel furnitureMasterModel = (furniture_model == null) ? null : furniture_model.Master;
+                if (furnitureMasterModel != null && furnitureMasterModel.Id == 66000)
+                {
+                    List<ICommand> list = new List<ICommand>();
+                    foreach (ICommand item in furniture_controller.Commands)
+                    {
+                        list.Add(item);
+                    }
+                    list.Add(new HarvestCommand());
+                    furniture_controller.SetCommand(list.ToArray());
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(AnimalModel))]
+        public static class AnimalModel_Patch
+        {
+            [HarmonyPatch("Snack")]
+            [HarmonyPrefix]
+            public static bool Snack_Prefix(AnimalModel __instance)
+            {
+                __instance.HasSnack = false;
+                return true;
+            }
+
+            [HarmonyPatch("Brush")]
+            [HarmonyPrefix]
+            public static bool Brush_Prefix(AnimalModel __instance)
+            {
+                __instance.IsBrushed = false;
+                return true;
+            }
+        }
+    }
+}
